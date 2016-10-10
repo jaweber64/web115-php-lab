4eef1cecb3c3ea9319696379d6f1f28f182e1aaa
jaweber64 <jaWeber@janets-mbp.attlocal.net>
2016-10-10 17:10:30 -0500

    Initial commit

---
+++ header.php
@@ -0,0 +1,9 @@
+<h1>Janet Weber PHP Practice</h1>
+<h3>(in HTML&CSS Class)</h3>
+
+
+<nav>
+    <a href="index.php">Page 1</a>
+    <a href="page2.php">Page 2</a>
+    <a href="page3.php">Page 3</a>
+</nav>
